79996afb9cdc40ffe9b1fb832ad6f33de441bf17
gorozco9 <gorozco9@stu.southtexascollege.edu>
2024-04-07 23:46:26 -0500

    create function

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,7 +1,29 @@
+import Employee from "../model/employeeModel.js"
+
+export const create = async(req, res) => {
+    try {
+        const employeeData = new Employee(req.body)
+        const { userName } = employeeData
+        const employeeExist = await Employee.findOne({userName})
+    
+        if(employeeExist) {
+            return res.staus(200).json({message: "Employee already exists."})
+        }
+
+        const saveEmployee = await employeeData.save();
+        res.status(200).json(savedEmployee)
+
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+
+
 export const fetch = async(req, res) => {
     try {
         res.json("Hello World")
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
-    }
+    }    
 }
